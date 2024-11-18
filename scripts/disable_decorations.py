8a3586cc87f606a21a223147782d20d4f52e920e
junnunkarim <24603630+junnunkarim@users.noreply.github.com>
2024-11-19 02:16:35 +0600

    added a script for getting battery info; added a battery widget to the lockscreen (hyprlock)

--- scripts/disable_decorations.py
+++ scripts/disable_decorations.py
@@ -45,6 +45,22 @@ def disable_all_decorations():
     subprocess.run(shlex.split(batch_cmd))
 
 
+def zen_mode():
+    batch_cmd = """hyprctl --batch "keyword animations:enabled 0;
+                    keyword decoration:drop_shadow 0;
+                    keyword decoration:blur:enabled 0;
+                    keyword general:gaps_in 2;
+                    keyword general:gaps_out 4;
+                    keyword decoration:rounding 0;
+                    keyword decoration:active_opacity 0.95;
+                    keyword decoration:inactive_opacity 0.85"
+                 """
+    subprocess.run(shlex.split(batch_cmd))
+    #
+    # bar_cmd = 'ags --toggle-window "bar-0"'
+    # subprocess.run(shlex.split(bar_cmd))
+
+
 # main function to handle the cli arguments
 def main():
     parser = argparse.ArgumentParser(description="Disable Hyprland decorations.")
@@ -53,6 +69,11 @@ def main():
     )
     parser.add_argument("--gaps", action="store_true", help="Disable only gaps")
     parser.add_argument("--all", action="store_true", help="Disable all decorations")
+    parser.add_argument(
+        "--zen",
+        action="store_true",
+        help="Disable all decorations and toggles the bar off",
+    )
 
     args = parser.parse_args()
 
@@ -64,8 +85,10 @@ def main():
             disable_gaps()
         elif args.all:
             disable_all_decorations()
+        elif args.zen:
+            zen_mode()
         else:
-            print("No valid option provided. Use --anim_blur or --all.\n")
+            print("No valid option provided. Use --anim_blur, --all or --zen.\n")
             parser.print_help()
     else:
         # reload default hyprland config if animations are not enabled
