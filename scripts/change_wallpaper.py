a29840d35b6eee675e485ef4890ffd098a3642fd
junnunkarim <24603630+junnunkarim@users.noreply.github.com>
2025-02-24 06:01:04 +0600

    routine update

--- scripts/change_wallpaper.py
+++ scripts/change_wallpaper.py
@@ -12,8 +12,8 @@ def change_wallpaper(wallpaper: str, directory: str = "~/.config/wallpaper/"):
     if directory[-1] != "/":
         directory = directory + "/"
 
-    print(wallpaper)
-    print(directory)
+    # print(wallpaper)
+    # print(directory)
     wall_directory = Path(directory + wallpaper).expanduser()
 
     cmd = [
